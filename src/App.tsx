37043f4eb3a3bac662992554c6beddea8d8e6561
mayakovsky <mayakovsky03@gmail.com>
2022-08-18 14:00:38 +0300

    Add New Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {NewComponent} from "./NewComponent";
+import {TopCarsComponent} from "./TopCarsComponent";
 
 function App() {
-  return (
-    <>
-      <NewComponent/>
-    </>
-  );
+
+    const topCars = [
+        {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Mercedes', model: 'e63s'},
+        {manufacturer: 'Audi', model: 'rs6'}
+    ]
+
+    let [students] = useState(
+        [
+            {id: 1, name: 'James', age: 8},
+            {id: 2, name: 'Robert', age: 18},
+            {id: 3, name: 'Ace', age: 28},
+            {id: 4, name: 'Bob', age: 38},
+            {id: 5, name: 'Frank', age: 5},
+            {id: 6, name: 'Julia', age: 34},
+            {id: 7, name: 'John', age: 100},
+            {id: 8, name: 'Pedro', age: 14},
+            {id: 9, name: 'Mike', age: 25},
+            {id: 10, name: 'Suzie', age: 35},
+            {id: 11, name: 'Bestie', age: 40},
+        ]
+    );
+    return (
+        <>
+            <NewComponent students={students}/>
+            <TopCarsComponent topCars={topCars}/>
+        </>
+
+    );
 }
+
 
 export default App;
--- src/NewComponent.tsx
+++ src/NewComponent.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 
-
-
-export const NewComponent = () => {
-    return (
-        <div>
-            new component
-        </div>
-    )
+type NewComponentType = {
+    // students: Array<StudentType>
+    students: StudentType[]
 }
+
+type StudentType = {
+    id: number,
+    name: string,
+    age: number
+}
+
+export const NewComponent = (props: NewComponentType) => {
+
+    return (
+        <ul>
+            {props.students.map((objectFromStudentArray, index) => {
+                return (
+                    <li key={objectFromStudentArray.id}>
+                        <span>{objectFromStudentArray.name}</span>
+                        <span> age: {objectFromStudentArray.age}</span>
+                    </li>
+                )
+            })}
+        </ul>
+            );
+            };
+
+
+
+
+
